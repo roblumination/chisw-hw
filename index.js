e50e025f45e8048f8f9b575ded0fbc40ff437dfe
Roman Savran <roblumination@gmail.com>
2022-09-13 19:32:38 +0300

    refactor according to comments

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import WeatherWidget from "./ww/scripts/WeatherWidget.js";
 const weatherElement = document.getElementById("weather-widget");
-if (weatherElement === null)
-    throw new Error();
+if (!weatherElement) throw new Error();
 const weatherWidget = new WeatherWidget(weatherElement);
 const container = document.querySelector(".super-buttons");
 if (container) {
-    container.querySelectorAll("button").forEach((button) => {
-        button.addEventListener("click", () => weatherWidget.setCity(button.innerText));
-    });
+  container.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", () =>
+      weatherWidget.setCity(button.innerText)
+    );
+  });
 }
